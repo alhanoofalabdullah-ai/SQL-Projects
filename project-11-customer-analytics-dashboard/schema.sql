9554581e7c4417dbc56bf7a7b7dcb23defe99205
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-04-23 14:36:06 +0300

    Create schema.sql

---
+++ project-11-customer-analytics-dashboard/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    city VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    amount DECIMAL(10,2),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
